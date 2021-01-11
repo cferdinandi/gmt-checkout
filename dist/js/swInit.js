922dd7c26291874f78d22a2c28291b6abe367b84
cferdinandi <chris@gomakethings.com>
2021-01-10 23:11:51 -0500

    Updated JS

--- dist/js/swInit.js
+++ dist/js/swInit.js
@@ -1,4 +1,4 @@
-/*! gmt-checkout-2 v2.0.0 | (c) 2020 Chris Ferdinandi | MIT License | https://github.com/cferdinandi/gmt-checkout | Open Source: undefined */
+/*! gmt-checkout v2.0.5 | (c) 2021 Chris Ferdinandi | MIT License | https://github.com/cferdinandi/gmt-checkout | Open Source: undefined */
 (function () {
 	'use strict';
 
