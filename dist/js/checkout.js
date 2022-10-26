acbca971950d7f3d77319d2f2a8301ea80685c43
cferdinandi <chris@gomakethings.com>
2022-10-26 11:30:39 -0400

    Save ref across session

--- dist/js/checkout.js
+++ dist/js/checkout.js
@@ -6,8 +6,11 @@
 
         // Get ref URL
         let url = new URL(window.location.href);
-        let ref = url.searchParams.get('ref');
+        let ref = url.searchParams.get('ref') || sessionStorage.getItem('gmt-checkout-ref');
         if (!ref) return;
+
+        // Save the ref
+        sessionStorage.setItem('gmt-checkout-ref', ref);
 
         // Get link to update
         let link = document.querySelector('[href*="/resources/"]');
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -2,8 +2,11 @@ function keepShopping () {
 
     // Get ref URL
     let url = new URL(window.location.href);
-    let ref = url.searchParams.get('ref');
+    let ref = url.searchParams.get('ref') || sessionStorage.getItem('gmt-checkout-ref');
     if (!ref) return;
+
+    // Save the ref
+    sessionStorage.setItem('gmt-checkout-ref', ref);
 
     // Get link to update
     let link = document.querySelector('[href*="/resources/"]');
