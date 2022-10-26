28eea13ab50eb071a526db92e8f2b41ce1e2216e
cferdinandi <chris@gomakethings.com>
2022-10-26 11:31:25 -0400

    Updated theme header

--- dist/js/swInit.js
+++ dist/js/swInit.js
@@ -1,4 +1,4 @@
-/*! gmt-checkout v3.4.0 | (c) 2022 Chris Ferdinandi | MIT License | https://github.com/cferdinandi/gmt-checkout | Open Source: undefined */
+/*! gmt-checkout v3.5.0 | (c) 2022 Chris Ferdinandi | MIT License | https://github.com/cferdinandi/gmt-checkout | Open Source: undefined */
 (function () {
 	'use strict';
 
